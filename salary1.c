6b27a8fad611b56f9210dafc422668f6338a771f
damned-coder <devenk729@gmail.com>
2025-08-14 15:51:02 +0530

    Add files via upload

---
+++ salary1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+int x ;
+int y ;
+int z ;
+printf("What is your salary?");
+scanf("%d",&x);
+
+if(x>=10000)
+{ y=10;
+printf("your allowance is %d",y);
+
+}
+else if (10000>x>5000)
+{
+    y=5;
+printf("your allowance is %d",y);
+}
+}
+
+
