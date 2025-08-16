6825b5b999a7ec01eb5585c2814efbb31ef5794b
damned-coder <devenk729@gmail.com>
2025-08-16 11:52:28 +0530

    Add files via upload

--- salary1.c
+++ salary1.c
@@ -1,24 +1,34 @@
 #include<stdio.h>
-#include<conio.h>
+#include<math.h>
 
 int main ()
 {
 int x ;
 int y ;
 int z ;
+int b ;
 printf("What is your salary?");
 scanf("%d",&x);
 
 if(x>=10000)
 { y=10;
-printf("your allowance is %d",y);
+  b=3;
+printf("your allowance is %d percent and your deduction is %d percent \n",y,b);
+z=x+x*y/100-x*b/100 ;
+printf("Your net salary is %d\n",z);
 
 }
-else if (10000>x>5000)
+else if (x>=5000 && x<10000)
 {
     y=5;
-printf("your allowance is %d",y);
+    b=2;
+printf("your allowance is %d percent and your deduction is %d percent \n",y,b);
+z=x+x*y/100-x*b/100;
+printf("Your net salary is %d",z);
+}
+else
+{
+printf("Your salary is too low to calculate allowences and deductions.");
 }
 }
-
 
