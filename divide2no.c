074142d98b3340efc7c72b6392c8c0ed631bb5a0
damned-coder <devenk729@gmail.com>
2025-08-20 18:50:24 +0530

    Add files via upload

---
+++ divide2no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+float main()
+{
+int x;
+float y;
+float z;
+printf("Give me two numbers and I will divide them.");
+scanf("%d%f",&x,&y);
+z=y/x;
+printf("The division of the numbers is %f",z);
+}
