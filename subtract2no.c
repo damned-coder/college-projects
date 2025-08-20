852402b9469b6dd20e1fd671a769b7f1cdbd90f2
damned-coder <devenk729@gmail.com>
2025-08-20 18:44:31 +0530

    Add files via upload

---
+++ subtract2no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+int x;
+int y;
+int z;
+printf("Give me two numbers and I will subtract them.");
+scanf("%d%d",&x,&y);
+z=y-x;
+printf("The difference of the numbers is %d",z);
+}
