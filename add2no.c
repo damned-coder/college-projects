b080739e86e1fafcd1cb9bb52e01b1fb21b5a018
damned-coder <devenk729@gmail.com>
2025-08-20 18:40:08 +0530

    Add files via upload

---
+++ add2no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+int x;
+int y;
+int z;
+printf("Give me two numbers and I will add them.");
+scanf("%d%d",&x,&y);
+z=y+x;
+printf("The sum of the number is %d",z);
+}
