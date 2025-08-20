d05f0a84b3202bd779c78b40f1d4f475333c4d29
damned-coder <devenk729@gmail.com>
2025-08-20 18:46:16 +0530

    Add files via upload

---
+++ multiply2no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+int x;
+int y;
+int z;
+printf("Give me two numbers and I will multiply them.");
+scanf("%d%d",&x,&y);
+z=y*x;
+printf("The product of the numbers is %d",z);
+}
